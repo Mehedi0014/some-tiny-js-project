87fa6e2caa3a348e93ca9ce0475faeaaadf304e6
Mehedi0014 <mehedi0014@gmail.com>
2022-01-01 13:09:09 +0600

    Two porject include:  1.Add custom link in elementor carousel or image gallery, 2.Find all images where alt tag aren't use.

---
+++ README.txt
@@ -0,0 +1,15 @@
+===================================================================
+>>> Porject 01
+>>> Add custom link in elementor carousel or image gallery
+===================================================================
+If your uppoer node is not anchor tag then this line: =>
+  let catchImageTagParentNote = imgNode.parentNode;
+arn't work. Then you inspect your code and find the anchor tag and modify the line like: =>
+  let catchImageTagParentNote = imgNode.parentNode.parentNode.parentNode;
+
+
+
+===================================================================
+>>> Porject 02
+>>> Find all images where alt tag aren't use
+===================================================================
---
+++ main.js
@@ -0,0 +1,50 @@
+/*
+===================================================================
+>>> Porject 01
+>>> Add custom link in elementor carousel or image gallery
+===================================================================
+*/
+function addCustomLinkInElementorCarouselOrGallery(){
+  let allImageAltCollection = ["Petopedia" , "kitchen gadgets"];
+  let allYoutubeUrlCollection = ["https://google.com" , "https://fb.com"];
+  let allImageAltCollectionLength = allImageAltCollection.length;
+
+  let allImages = document.getElementsByTagName("img");
+  for( let allImage in allImages){
+    let altName = allImages[allImage]["alt"];
+    if(allImageAltCollection.includes(altName)){
+      let altIndexNumber = allImageAltCollection.indexOf(altName);
+      let imgNode = allImages[allImage];
+      let catchImageTagParentNote = imgNode.parentNode;
+      let catchImageTagParentNoteUrl = catchImageTagParentNote.href;
+      if (catchImageTagParentNoteUrl !== null && catchImageTagParentNoteUrl !== '') {
+        catchImageTagParentNote.href = allYoutubeUrlCollection[altIndexNumber];
+      }
+    }
+  }
+}
+addCustomLinkInElementorCarouselOrGallery();
+
+
+
+
+/*
+===================================================================
+>>> Porject 02
+>>> Find all images where alt tag aren't use
+===================================================================
+*/
+function getImgNodeWhereWeAreNotUseAlt(){
+  let allImages = document.getElementsByTagName("img");
+  let len = allImages.length;
+  let images = [];
+
+  for( let i = 0; i <= len; i++){
+    let blankAlt = allImages[i].alt;
+    let blankSrc = allImages[i].src;
+    if (blankAlt == ""){
+      console.log(blankSrc);
+    }
+  }
+}
+getImgNodeWhereWeAreNotUseAlt();
